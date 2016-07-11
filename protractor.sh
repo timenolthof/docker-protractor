b96299fd5f21583dd19b4a12b3ca351b404f5c57
Niels Dequeker <niels.dequeker@gmail.com>
2016-07-11 16:51:10 +0200

    choreL Use concurrently to run xvfb and tests

--- protractor.sh
+++ protractor.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-xvfb-run --server-args='-screen 0 1280x1024x24' protractor $@
+set -ex
+
+export DISPLAY=:99
+
+concurrent --kill-others \
+  '/usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset' \
+  'protractor ./protractor.conf.js'
