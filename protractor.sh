f605ff6c76518cea99d16dd44ce02b78e4f002c8
Niels Dequeker <niels.dequeker@gmail.com>
2016-07-11 16:53:29 +0200

    refactor: Replace deprecated concurrent command with concurrently

--- protractor.sh
+++ protractor.sh
@@ -4,6 +4,6 @@ set -ex
 
 export DISPLAY=:99
 
-concurrent --kill-others \
+concurrently --kill-others \
   '/usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset' \
   'protractor ./protractor.conf.js'
